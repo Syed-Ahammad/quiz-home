47ad46f835ce01580c00ee2599780ab914d59e42
syed-ahammad <Syed_Ahammad@outlook.com>
2022-10-12 00:08:49 +0600

    recharts added

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { TopicContext } from "../../layouts/Main";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
 
 const Statistics = () => {
-    return (
-        <div>
-            <h1>this is statistics component</h1>
-        </div>
-    );
+  const quizTopics = useContext(TopicContext);
+  const data = quizTopics.data;
+  console.log(quizTopics)
+  return (
+    <div>
+      <h1>this is statistics component</h1>
+      <BarChart width={800} height={400} data={data}>
+        <XAxis dataKey="name" stroke="#8884d8" />
+        <YAxis />
+        <Tooltip wrapperStyle={{ width: 100, backgroundColor: "#ccc" }} />
+        <Legend
+          width={100}
+          wrapperStyle={{
+            top: 40,
+            right: 20,
+            backgroundColor: "#f5f5f5",
+            border: "1px solid #d5d5d5",
+            borderRadius: 3,
+            lineHeight: "40px",
+          }}
+        />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <Bar dataKey="total" fill="#8884d8" barSize={30} />
+      </BarChart>
+    </div>
+  );
 };
 
 export default Statistics;
