cd7dfce0ca8c0a1396f88a6d504348348a37d628
syed-ahammad <Syed_Ahammad@outlook.com>
2022-10-11 12:33:31 +0600

    card and question component added

---
+++ src/components/questions/Questions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Questions = () => {
+    return (
+        <div>
+            <h1>questions component</h1>
+        </div>
+    );
+};
+
+export default Questions;
